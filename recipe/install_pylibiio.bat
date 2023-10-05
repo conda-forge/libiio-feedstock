0301bbdd0a9df715171f4d507b00a5b9ef80ae8d
Ryan Volz <ryan.volz@gmail.com>
2023-10-05 15:49:42 -0400

    Try to fix library detection when building python bindings.

--- recipe/install_pylibiio.bat
+++ recipe/install_pylibiio.bat
@@ -7,10 +7,11 @@ mkdir build
 cd build
 
 :: configure
+:: set CMAKE_INSTALL_FULL_LIBDIR, running from here means it is not set by default
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_FULL_LIBDIR="%LIBRARY_LIB%" ^
+    -DCMAKE_INSTALL_FULL_LIBDIR="%LIBRARY_BIN%" ^
     -DPython_EXECUTABLE:PATH="%PYTHON%" ^
     -DWITH_DOC=OFF ^
     -DVERSION=%PKG_VERSION% ^
@@ -22,4 +23,4 @@ cmake --build . --config Release
 if errorlevel 1 exit 1
 
 :: don't install with CMake, rather install with pip to avoid .egg-info dir
-%PYTHON% -m pip install . -vv
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
--- recipe/install_pylibiio.sh
+++ recipe/install_pylibiio.sh
@@ -6,6 +6,7 @@ cd bindings/python
 mkdir build
 cd build
 
+# set CMAKE_INSTALL_FULL_LIBDIR, running from here means it is not set by default
 cmake_config_args=(
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_PREFIX=$PREFIX
@@ -15,8 +16,15 @@ cmake_config_args=(
     -DVERSION=$PKG_VERSION
 )
 
+if [[ $target_platform == osx* ]] ; then
+    # set CMAKE_CROSSCOMPILING to short-circuit broken installed library detection
+    cmake_config_args+=(
+        -DCMAKE_CROSSCOMPILING=ON
+    )
+fi
+
 cmake ${CMAKE_ARGS} .. "${cmake_config_args[@]}"
 cmake --build . --config Release
 
 # don't install with CMake, rather install with pip to avoid .egg-info dir
-$PYTHON -m pip install . -vv
+$PYTHON -m pip install . --no-deps --no-build-isolation -vv
